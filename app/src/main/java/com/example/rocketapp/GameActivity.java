09550dd7137fca1eba7c39da9c6cf45b3995048b
Barak-Kuzi <barakkuzi1997@gmail.com>
2024-06-28 19:39:49 +0300

    Task2 - Updated 28/6

---
+++ app/src/main/java/com/example/rocketapp/GameActivity.java
@@ -0,0 +1,146 @@
+package com.example.rocketapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class GameActivity extends AppCompatActivity {
+
+    private ShapeableImageView[] lives;
+    private ShapeableImageView[] houses;
+    private ShapeableImageView[][] rockets;
+    private ShapeableImageView[][] israel_coins;
+    private AppCompatImageButton left, right;
+    private TextView odometer;
+    private Handler handler;
+    private Runnable runnable;
+
+    private final int ROWS = 8;
+    private final int COLS = 5;
+    private final int LIVES = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        startGame();
+    }
+
+    public void moveLeft(View view) {
+        GameManager.getInstance().moveHouse("left");
+        updateHouse();
+    }
+
+    public void moveRight(View view) {
+        GameManager.getInstance().moveHouse("right");
+        updateHouse();
+    }
+
+    public void updateHouse() {
+
+        int index = GameManager.getInstance().getHouseIndex();
+
+        for (int i = 0; i < COLS; i++) {
+            houses[i].setVisibility(i == GameManager.getInstance().getHouseIndex() ? View.VISIBLE : View.INVISIBLE);
+        }
+    }
+
+    public void init() {
+        GameManager.init(this);
+        lives = new ShapeableImageView[]{
+                findViewById(R.id.heart1),
+                findViewById(R.id.heart2),
+                findViewById(R.id.heart3)
+        };
+
+        houses = new ShapeableImageView[]{
+                findViewById(R.id.house0),
+                findViewById(R.id.house1),
+                findViewById(R.id.house2),
+                findViewById(R.id.house3),
+                findViewById(R.id.house4),
+        };
+
+        rockets = new ShapeableImageView[][]{
+                {findViewById(R.id.rocket0), findViewById(R.id.rocket1), findViewById(R.id.rocket2), findViewById(R.id.rocket3), findViewById(R.id.rocket4)},
+                {findViewById(R.id.rocket5), findViewById(R.id.rocket6), findViewById(R.id.rocket7), findViewById(R.id.rocket8), findViewById(R.id.rocket9)},
+                {findViewById(R.id.rocket10), findViewById(R.id.rocket11), findViewById(R.id.rocket12), findViewById(R.id.rocket13), findViewById(R.id.rocket14)},
+                {findViewById(R.id.rocket15), findViewById(R.id.rocket16), findViewById(R.id.rocket17), findViewById(R.id.rocket18), findViewById(R.id.rocket19)},
+                {findViewById(R.id.rocket20), findViewById(R.id.rocket21), findViewById(R.id.rocket22), findViewById(R.id.rocket23), findViewById(R.id.rocket24)},
+                {findViewById(R.id.rocket25), findViewById(R.id.rocket26), findViewById(R.id.rocket27), findViewById(R.id.rocket28), findViewById(R.id.rocket29)},
+                {findViewById(R.id.rocket30), findViewById(R.id.rocket31), findViewById(R.id.rocket32), findViewById(R.id.rocket33), findViewById(R.id.rocket34)},
+                {findViewById(R.id.rocket35), findViewById(R.id.rocket36), findViewById(R.id.rocket37), findViewById(R.id.rocket38), findViewById(R.id.rocket39)}
+        };
+
+        israel_coins = new ShapeableImageView[][]{
+                {findViewById(R.id.israel_coin0), findViewById(R.id.israel_coin1), findViewById(R.id.israel_coin2), findViewById(R.id.israel_coin3), findViewById(R.id.israel_coin4)},
+                {findViewById(R.id.israel_coin5), findViewById(R.id.israel_coin6), findViewById(R.id.israel_coin7), findViewById(R.id.israel_coin8), findViewById(R.id.israel_coin9)},
+                {findViewById(R.id.israel_coin10), findViewById(R.id.israel_coin11), findViewById(R.id.israel_coin12), findViewById(R.id.israel_coin13), findViewById(R.id.israel_coin14)},
+                {findViewById(R.id.israel_coin15), findViewById(R.id.israel_coin16), findViewById(R.id.israel_coin17), findViewById(R.id.israel_coin18), findViewById(R.id.israel_coin19)},
+                {findViewById(R.id.israel_coin20), findViewById(R.id.israel_coin21), findViewById(R.id.israel_coin22), findViewById(R.id.israel_coin23), findViewById(R.id.israel_coin24)},
+                {findViewById(R.id.israel_coin25), findViewById(R.id.israel_coin26), findViewById(R.id.israel_coin27), findViewById(R.id.israel_coin28), findViewById(R.id.israel_coin29)},
+                {findViewById(R.id.israel_coin30), findViewById(R.id.israel_coin31), findViewById(R.id.israel_coin32), findViewById(R.id.israel_coin33), findViewById(R.id.israel_coin34)},
+                {findViewById(R.id.israel_coin35), findViewById(R.id.israel_coin36), findViewById(R.id.israel_coin37), findViewById(R.id.israel_coin38), findViewById(R.id.israel_coin39)}
+        };
+
+        odometer = findViewById(R.id.odometer);
+        right = findViewById(R.id.right);
+        right.setOnClickListener(this::moveRight);
+        left = findViewById(R.id.left);
+        left.setOnClickListener(this::moveLeft);
+    }
+
+    public void updateLives() {
+        int currentLives = GameManager.getInstance().getLives();
+        for (int i = 0; i < LIVES; i++) {
+            lives[i].setVisibility(currentLives >= i + 1 ? View.VISIBLE : View.INVISIBLE);
+        }
+    }
+
+    public void timeTick() {
+        GameManager.getInstance().setRoad();
+        GameManager.getInstance().setRocketOrIsraelCoin();
+        updateUI();
+    }
+
+    public void startGame() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                timeTick();
+                handler.postDelayed(this, 800);
+            }
+        };
+        handler.postDelayed(runnable, 800);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void updateUI() {
+        int[][] road = GameManager.getInstance().getRoad();
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (road[i][j] > 0) {
+                    if (road[i][j] == 1)
+                        rockets[i][j].setVisibility(View.VISIBLE);
+                    else
+                        israel_coins[i][j].setVisibility(View.VISIBLE);
+                } else {
+                    rockets[i][j].setVisibility(View.INVISIBLE);
+                    israel_coins[i][j].setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+        odometer.setText("Odometer: " + GameManager.getInstance().getOdometer());
+        updateLives();
+    }
+
+}
--- app/src/main/java/com/example/rocketapp/GameManager.java
+++ app/src/main/java/com/example/rocketapp/GameManager.java
@@ -6,19 +6,20 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.widget.Toast;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class GameManager {
 
     @SuppressLint("StaticFieldLeak")
     public static GameManager game;
-    private int lives = 3;
+    private int lives = 3, score = 0, odometer = 0;
     private boolean isGameOver = false;
     private final Context context;
 
-    private final boolean[][] road = new boolean[6][3];
-    private int houseIndex = 1;
+    private final int ROWS = 8;
+    private final int COLS = 5;
+    private final int[][] road = new int[ROWS][COLS];
+    private int houseIndex = 2; // initial to center;
 
     private GameManager(Context context) {
         this.context = context;
@@ -31,15 +32,19 @@ public class GameManager {
                 houseIndex--;
             }
         } else if (Objects.equals(movement, "right")) {
-            if (houseIndex < 2) {
+            if (houseIndex < 4) {
                 houseIndex++;
             }
         }
     }
 
-    public void setRocket() {
-        int random = (int) (Math.random() * 3);
-        this.road[0][random] = true;
+    public void setRocketOrIsraelCoin() {
+        int randomCol = (int) (Math.random() * COLS);
+        int rocketOrIsraelCoin = (int) (Math.random() * 3); // 0/1 = rocket, 2 = israel_coin
+        if (rocketOrIsraelCoin != 2)
+            this.road[0][randomCol] = 1;
+        else
+            this.road[0][randomCol] = 2;
     }
 
 
@@ -52,36 +57,46 @@ public class GameManager {
         return game;
     }
 
-
     public void initRoad() {
-        for (boolean[] booleans : road) {
-            Arrays.fill(booleans, false);
-        }
-    }
-
-    public void setRoad() {
-        for (int i = 5; i >= 0; i--) {
-            for (int j = 0; j < 3; j++) {
-
-                if (rocketExists(i, j)) {
-                    if (i == 5) {
-                        if (houseIndex == j)
-                            boom();
-                    } else {
-                        this.road[i + 1][j] = this.road[i][j];
-                    }
-                }
-                road[i][j] = false;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                road[i][j] = 0;
             }
         }
     }
 
-    public boolean rocketExists(int i, int j) {
-        return this.road[i][j];
+    public void setRoad() {
+        updateOdometer();
+        for (int i = ROWS - 1; i >= 0; i--) {
+            for (int j = 0; j < COLS; j++) {
+                if (this.road[i][j] != 0) {
+                    if (i == ROWS - 1) {
+                        if (houseIndex == j) {
+                            if (this.road[i][j] == 1) // A collision with a rocket
+                                boom();
+                            else if (this.road[i][j] == 2) // A collision with a coin
+                                updateScore();
+                        }
+                    } else {
+                        this.road[i + 1][j] = this.road[i][j];
+                    }
+                }
+                road[i][j] = 0;
+            }
+        }
+    }
+
+    private void updateScore() {
+        this.score += 10;
+        toast("You gained a points\nYour score: " + this.score);
+    }
+
+    private void updateOdometer() {
+        this.odometer += 1;
     }
 
     public void boom() {
-        lives--;
+        this.lives--;
         if (lives == 0) {
             isGameOver = true;
             toast("You lost the game!");
@@ -95,7 +110,9 @@ public class GameManager {
 
 
     public void restartGame() {
-        lives = 3;
+        this.lives = 3;
+        this.score = 0;
+        this.odometer = 0;
         isGameOver = false;
         initRoad();
     }
@@ -107,6 +124,10 @@ public class GameManager {
     public void vibrate() {
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+    }
+
+    public int getOdometer() {
+        return this.odometer;
     }
 
     public int getLives() {
@@ -121,7 +142,7 @@ public class GameManager {
         return context;
     }
 
-    public boolean[][] getRoad() {
+    public int[][] getRoad() {
         return road;
     }
 
